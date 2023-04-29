4f1e7b0695d516b1ca752e9e91e9312c336e9339
Ainadinov01~ <a.adilet.97@mail.ru>
2023-04-29 16:26:13 +0600

    quiz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Counter from './component/counter/counter';
 import Modal from './component/modal/modal';
+import Quiz from './component/quiz/quiz';
 
 function App() {
   return (
     <>
       {/* <Counter/> // Счётчик */}
-      <Modal/>
+      {/* <Modal/> */}
+      <Quiz/>
     </>
   );
 }
